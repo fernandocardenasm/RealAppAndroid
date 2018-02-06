d6d5eba8fe1084a1c6ff4f8f12072f323eb68bf1
Fernando <fernando@Raffaelles-MBP.fritz.box>
2018-02-06 20:39:10 +0100

    Last user study

--- app/src/main/java/com/example/fernando/realapp/SeedData.java
+++ app/src/main/java/com/example/fernando/realapp/SeedData.java
@@ -15,47 +15,47 @@ public class SeedData {
         person1.imageProfile = "claudia_p";
         person1.imageSelf = "claudia_s";
         person1.imageFriend = "claudia_f";
-        person1.listFriends.addAll(Arrays.asList(""));
+        person1.listFriends.addAll(Arrays.asList("4", "5", "6"));
 
         Person person2 = new Person();
         person2.userId = "2";
-        person2.userName = "Mark";
-        person2.imageProfile = "mark_p";
-        person2.imageSelf = "mark_s";
-        person2.imageFriend = "mark_f";
-        person2.listFriends.addAll(Arrays.asList("3"));
+        person2.userName = "Razan";
+        person2.imageProfile = "razan_p";
+        person2.imageSelf = "razan_s";
+        person2.imageFriend = "razan_s";
+        person2.listFriends.addAll(Arrays.asList(""));
 
         Person person3 = new Person();
         person3.userId = "3";
-        person3.userName = "Emilio";
-        person3.imageProfile = "emilio_p";
-        person3.imageSelf = "emilio_s";
-        person3.imageFriend = "emilio_f";
-        person3.listFriends.addAll(Arrays.asList("4"));
+        person3.userName = "Mihaela";
+        person3.imageProfile = "mihaela_p";
+        person3.imageSelf = "mihaela_s";
+        person3.imageFriend = "mihaela_s";
+        person3.listFriends.addAll(Arrays.asList(""));
 
         Person person4 = new Person();
         person4.userId = "4";
-        person4.userName = "Gabriela";
-        person4.imageProfile = "gabi_p";
-        person4.imageSelf = "gabi_s";
-        person4.imageFriend = "gabi_f";
-        person4.listFriends.addAll(Arrays.asList("2","3"));
+        person4.userName = "Emilio";
+        person4.imageProfile = "emilio_p";
+        person4.imageSelf = "emilio_s";
+        person4.imageFriend = "emilio_f";
+        person4.listFriends.addAll(Arrays.asList("1","5","6"));
 
         Person person5 = new Person();
         person5.userId = "5";
-        person5.userName = "Djanina";
-        person5.imageProfile = "dj_p";
-        person5.imageSelf = "dj_s";
-        person5.imageFriend = "dj_s";
-        person5.listFriends.addAll(Arrays.asList("0"));
+        person5.userName = "Azusena";
+        person5.imageProfile = "azu_p";
+        person5.imageSelf = "azu_s";
+        person5.imageFriend = "azu_f";
+        person5.listFriends.addAll(Arrays.asList("1", "4", "6"));
 
         Person person6 = new Person();
         person6.userId = "6";
-        person6.userName = "Rana";
-        person6.imageProfile = "rana_p";
-        person6.imageSelf = "rana_s";
-        person6.imageFriend = "rana_s";
-        person6.listFriends.addAll(Arrays.asList("0"));
+        person6.userName = "Sandra";
+        person6.imageProfile = "person1_f";
+        person6.imageSelf = "sandra_s";
+        person6.imageFriend = "sandra_f";
+        person6.listFriends.addAll(Arrays.asList("1", "4", "5"));
 
 
         Person[] people = {person1, person2, person3, person4, person5, person6};
